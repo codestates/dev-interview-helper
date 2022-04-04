cb3413f13c7491856f54cfdabb5def423bac7880
sungmin-choi <namja306@naver.com>
2022-04-05 01:32:15 +0900

    test) eslint 적용 테스트

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 function App() {
+  var a= 1;
   return (
-    <div className='App'>
+    <div className="App">
       <h1>dev-interview-helper</h1>
-      <p>dddddd</p>
-      <p>여기는 내 레퍼지토리!!!</p>
+      <p>아무거나</p>
     </div>
   );
 }
