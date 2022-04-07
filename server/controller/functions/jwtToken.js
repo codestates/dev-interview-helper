87c14de246c87ec1ef91b721991935d6d4ef0a29
adrienne20 <iridescent2103@gmail.com>
2022-04-07 23:06:55 +0900

    feat) jwtlogin

--- server/controller/functions/jwtToken.js
+++ server/controller/functions/jwtToken.js
@@ -3,9 +3,11 @@ const jwt = require("jsonwebtoken");
 
 module.exports = {
   generateAccessToken: (data) => {
+    //  Access token으로 sign
     return jwt.sign(data, process.env.ACCESS_SECRET, { expiresIn: "12h" });
   },
   sendAccessToken: (res, accessToken) => {
+    // JWT 토큰을 쿠키로 전달
     const cookieOptions = {
       maxAge: 900000,
       httpOnly: true,
@@ -13,6 +15,7 @@ module.exports = {
     res.cookie("accessToken", accessToken, cookieOptions);
   },
   checkAccessToken: (accessToken) => {
+    // JWT 토큰 정보를 받아서 검증
     try {
       return jwt.verify(accessToken, process.env.ACCESS_SECRET);
     } catch (err) {
